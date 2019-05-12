ab1039b8ff79ed40716074bb7f6b41359a7f4f3c
Clement Lakhal <clementlakhal@gmail.com>
2019-05-12 03:06:35 +0200

    Add commands

--- index.js
+++ index.js
@@ -1,80 +1,92 @@
-const program = '++++++++++[>+++++++>++++++++++>+++>++++<<<<-]>++.>+.+++++++..+++.>>++++.<++.<++++++++.--------.+++.------.--------.>+.';
+const fs = require('fs');
+const commander = require('commander');
 
-const MEMORY_SIZE = 30000;
-const memory = new Array(MEMORY_SIZE).fill(0);
+commander
+  .version('0.1.0');
 
-let index = 0;
-let pointer = 0;
+commander
+  .command('*')
+  .action((path) => {
+    const program = fs.readFileSync(path, 'utf8');
 
-const loops = [];
+    const MEMORY_SIZE = 30000;
+    const memory = new Array(MEMORY_SIZE).fill(0);
 
-let output = '';
+    let index = 0;
+    let pointer = 0;
 
-while (index !== program.length) {
-  switch (program[index]) {
-    case '+':
-      if (memory[pointer] === 255) {
-        memory[pointer] = 0;
-      } else {
-        memory[pointer] += 1;
-      }
-      break;
-    case '-':
-      if (memory[pointer] === 0) {
-        memory[pointer] = 255;
-      } else {
-        memory[pointer] -= 1;
-      }
-      break;
-    case '>':
-      if (pointer === MEMORY_SIZE - 1) {
-        pointer = 0;
-      } else {
-        pointer += 1;
-      }
-      break;
-    case '<':
-      if (pointer === 0) {
-        pointer = MEMORY_SIZE - 1;
-      } else {
-        pointer -= 1;
-      }
-      break;
-    case '[':
-      if (memory[pointer] !== 0) {
-        loops.push(index);
-      } else {
-        let count = 0;
-        let stopLoop = false;
+    const loops = [];
 
-        while (!stopLoop) {
-          index += 1;
+    let output = '';
 
-          if (index === program.length) {
-            stopLoop = true;
-          } else if (program[index] === '[') {
-            count += 1;
-          } else if (program[index] === ']') {
-            if (count === 0) {
-              stopLoop = true;
-            } else {
-              count -= 1;
+    while (index !== program.length) {
+      switch (program[index]) {
+        case '+':
+          if (memory[pointer] === 255) {
+            memory[pointer] = 0;
+          } else {
+            memory[pointer] += 1;
+          }
+          break;
+        case '-':
+          if (memory[pointer] === 0) {
+            memory[pointer] = 255;
+          } else {
+            memory[pointer] -= 1;
+          }
+          break;
+        case '>':
+          if (pointer === MEMORY_SIZE - 1) {
+            pointer = 0;
+          } else {
+            pointer += 1;
+          }
+          break;
+        case '<':
+          if (pointer === 0) {
+            pointer = MEMORY_SIZE - 1;
+          } else {
+            pointer -= 1;
+          }
+          break;
+        case '[':
+          if (memory[pointer] !== 0) {
+            loops.push(index);
+          } else {
+            let count = 0;
+            let stopLoop = false;
+
+            while (!stopLoop) {
+              index += 1;
+
+              if (index === program.length) {
+                stopLoop = true;
+              } else if (program[index] === '[') {
+                count += 1;
+              } else if (program[index] === ']') {
+                if (count === 0) {
+                  stopLoop = true;
+                } else {
+                  count -= 1;
+                }
+              }
             }
           }
-        }
+          break;
+        case ']':
+          index = loops.pop() - 1;
+          break;
+        case '.':
+          output += String.fromCharCode(memory[pointer]);
+          break;
+        default:
+          break;
       }
-      break;
-    case ']':
-      index = loops.pop() - 1;
-      break;
-    case '.':
-      output += String.fromCharCode(memory[pointer]);
-      break;
-    default:
-      break;
-  }
 
-  index += 1;
-}
+      index += 1;
+    }
 
-console.log(output);
+    console.log(output);
+  });
+
+commander.parse(process.argv);
